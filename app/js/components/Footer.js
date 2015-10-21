6a8260a840bc2f36494cb4fd65c4b558e6f8905b
Joel <joelalovera@gmail.com>
2015-10-21 19:30:40 -0300

    add events to footer

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -8,10 +8,14 @@ class Footer extends Component {
       super(props);
   }
 
+  _handleClick(text) {
+    ga('send', 'event', 'button', 'footer', text);
+  }
+
   render() {
     let style = !this.props.tracks ? 'footer fixed' : 'footer';
     return  <div className={style}>
-              <div className='copy'>Developed by <a href='https://github.com/loverajoel' target='_blank'>@loverajoel</a> / Designed by <a href='https://dribbble.com/curva' target='_blank'>@aschelstraete</a> / <a href='#'>Source Code</a></div>
+              <div className='copy'>Developed by <a href='https://github.com/loverajoel' target='_blank' onClick={this._handleClick.bind(this, '@loverajoel')}>@loverajoel</a> / Designed by <a href='https://dribbble.com/curva' target='_blank' onClick={this._handleClick.bind(this, '@aschelstraete')}>@aschelstraete</a> / <a href='#' onClick={this._handleClick.bind(this, 'github')}>Source Code</a></div>
               <div className='spotify-api'>Created using the API of <a href='https://developer.spotify.com/web-api/' target='_blank'><img src='img/spotify-logo.png'/></a></div>
             </div>
   }
