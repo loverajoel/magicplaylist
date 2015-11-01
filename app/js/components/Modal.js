0104d7c3ec7b1bd7d7f0020ae17ac44579939a43
Joel <joelalovera@gmail.com>
2015-11-01 12:39:42 -0300

    validate palylist name and show error if is false

--- app/js/components/Modal.js
+++ app/js/components/Modal.js
@@ -15,6 +15,7 @@ class Modal extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputError: false,
       playlistName: '',
       playlistPublic: true
     };
@@ -30,18 +31,28 @@ class Modal extends Component {
   }
 
   _savePlaylist() {
+    close();
+    save(
+      UserStore.getUser()._id,
+      playlistName,
+      this.state.playlistPublic, PlaylistStore.getTracks()
+    );
+  }
+
+  _validateForm() {
     const playlistName = ReactDOM.findDOMNode(this.refs.playlistName).value;
-    if (playlistName.length > 3) {
-      close();
-      save(
-        UserStore.getUser()._id,
-        playlistName,
-        this.state.playlistPublic, PlaylistStore.getTracks()
-      );
-    }
+    let isValid = playlistName.length > 3;
+    this.setState({
+      inputError: !isValid
+    });
+
+    return isValid;
   }
 
   _handleSave() {
+    if (!this._validateForm()) {
+      return;
+    }
     if (this.props.token &&
         this.props.user &&
         Number(localStorage.magic_token_expires) > Date.now())
@@ -62,6 +73,8 @@ class Modal extends Component {
   }
 
   render() {
+    let inputPlaceholder = this.state.inputError ? 'Please enter a valid name!' : 'Name';
+    let inputClass  = this.state.inputError ? 'playlist-name error' : 'playlist-name';
     return <div className='modal'>
               <div className='modal-container'>
                   <div className='close-modal'>
@@ -70,8 +83,8 @@ class Modal extends Component {
                   <div>
                     <input
                       type='text'
-                      placeholder='Name'
-                      className='playlist-name'
+                      placeholder={inputPlaceholder}
+                      className={inputClass}
                       ref='playlistName'
                     />
                   </div>
